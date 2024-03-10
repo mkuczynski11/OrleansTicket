c79b4b3992b408b917c0c35b31cc94e18f1da55b
mkuczynski11 <59289239+mkuczynski11@users.noreply.github.com>
2024-03-10 14:02:09 +0100

    external db persistence

--- OrleansTicket/Program.cs
+++ OrleansTicket/Program.cs
@@ -10,7 +10,14 @@ builder.Services.AddSwaggerGen();
 builder.Host.UseOrleans(static siloBuilder =>
 {
     siloBuilder.UseLocalhostClustering();
-    siloBuilder.AddMemoryGrainStorage("users");
+    //siloBuilder.AddMemoryGrainStorage("users");
+    siloBuilder.AddAdoNetGrainStorage("users", options =>
+    {
+        // RUN THIS https://github.com/dotnet/orleans/blob/main/src/AdoNet/Shared/PostgreSQL-Main.sql
+        // RUN THIS https://github.com/dotnet/orleans/blob/main/src/AdoNet/Orleans.Persistence.AdoNet/PostgreSQL-Persistence.sql
+        options.Invariant = "Npgsql";
+        options.ConnectionString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=admin;";
+    });
 });
 
 var app = builder.Build();
--- OrleansTicket/obj/Debug/net8.0/OrleansTicket.AssemblyInfo.cs
+++ OrleansTicket/obj/Debug/net8.0/OrleansTicket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrleansTicket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc7744c6ecb03e3f136c0ba4860899019231c39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5252cc197e7f0e172e54a8887ef451683f49e913")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrleansTicket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrleansTicket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
